203f21a7ccfc51f019fabba002fc43ee2d4b6d6e
qiyoudaoyi <qiyoudaoyi@gmail.com>
2021-11-10 21:51:34 +0800

    lec 2

---
+++ JSCourse/index.js
@@ -0,0 +1,33 @@
+const my_name = 'Wilson';   // srting
+const my_age = 20;          // number
+const my_hobby = "coding";  // string
+
+// in c: 
+// int my_age = 20;
+// char my_name[7] = "Wilson";
+
+// const str = "i'm " + my_name; // i'm Wilson
+
+// document.write('I am ' + my_name + ', and I am ' + my_age + ' years old, and I like ' + my_hobby + '.');
+// document.write('<br>');
+document.write(`<h1>I am ${my_name}.<br></h1>`);
+// document.write('<h1>' + 'I am ' + my_name);
+// document.write(', and I am ' + my_age + ' years old');
+// document.write(', and I like ' + my_hobby + '.' + '</h1>');
+
+
+// // I hope you still remember Pythagorean theorem
+// let a = 3;
+// let b = 4;
+// let c;
+
+// c = Math.sqrt(a**2 + b**2);
+// document.write(`<h1>a = ${a}, b = ${b}<br>`);
+// document.write(`c = ${c}<br>`);
+
+// // Random number
+// let random_num, random_die;
+// random_num = Math.random();
+// random_die = Math.floor(Math.random() * 6 + 1);
+// document.write(`random_num = ${random_num}<br/>`);
+// document.write(`random_die = ${random_die}<br/>`);`
